de2986169f8af6f3c1bf6d648b03427f095e2e22
huynhphong1611 <huynhvanphong123456@gmail.com>
2021-03-14 18:26:39 +0700

    Sua lại cách gọi connection tránh new nhiều lên

--- src/main/java/com/spring/dbcontext/DbContext.java
+++ src/main/java/com/spring/dbcontext/DbContext.java
@@ -4,16 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbContext {
-
-    private static final String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//    private static String DB_URL = "jdbc:sqlserver://localhost;"
+//            + "databaseName=SHOPLAPTOP;"
+//            + "integratedSecurity=true";
+    private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String connString = "jdbc:sqlserver://localhost:1433;databaseName=SHOPLAPTOP;instance=SQLSERVER;encrypt=true;trustServerCertificate=true";
     private static final String username = "sa";
     private static final String password = "123";
+    private static Connection sConnection;
 
     public static Connection getConnection() {
         try {
-            Class.forName(driver);
-            return DriverManager.getConnection(connString, username, password);
+            Class.forName(DRIVER);
+            if (sConnection ==  null) {
+                sConnection = DriverManager.getConnection(connString, username, password);
+            }
+            return sConnection;
+            //return DriverManager.getConnection(connString, username, password);
         } catch (Exception e) {
             throw new RuntimeException("Connecting database error", e);
             //return null;
